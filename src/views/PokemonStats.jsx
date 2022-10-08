1d0a4bf910cc7c3efdf5e02a6662e93eb7d6eff6
Pablo Goeppinger <pgoeppin@gmail.com>
2022-10-08 09:29:46 -0300

    Fixing deploy problems

--- src/views/PokemonStats.jsx
+++ src/views/PokemonStats.jsx
@@ -1,19 +1,64 @@
-import Cards from "../components/Cards";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
-
+import Card from "react-bootstrap/Card";
+import React from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 
 export default function PokemonStats() {
-
-
-    return (
-      <div className="mt-5 d-flex flex-column align-items-center">
-        <Cards />
-        <Link to="/pokemons">
+    const { pokeName } = useParams();
+  const [pokemon, setPokemon] = React.useState([]);
+  React.useEffect(() => {
+    
+    const PokeDetails = async () => {
+      try {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
+        setPokemon(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    PokeDetails();
+  }, [pokeName]);
+  return (
+    <div className="mt-5 d-flex flex-column align-items-center">
+      {
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+          />
+          <Card.Body>
+            <Card.Title className="text-center">
+              {pokemon.name? pokemon.name.charAt(0).toUpperCase()
+               + pokemon.name.slice(1) : undefined}
+            </Card.Title>
+            <Card.Subtitle className="my-2 text-center">
+                Type : 
+            {pokemon.types
+              ? pokemon.types.map((element, i) => (
+                  <span key={i}>
+                    {" " + element.type.name.charAt(0).toUpperCase() +
+                      element.type.name.slice(1) + " "}
+                  </span>
+                ))
+              : undefined}
+            </Card.Subtitle>
+            {pokemon.stats
+              ? pokemon.stats.map((element, i) => (
+                  <Card.Text key={i} className="m-0">
+                    {element.stat.name} : {element.base_stat}
+                  </Card.Text>
+                ))
+              : undefined}
+          </Card.Body>
+        </Card>
+        
+      }
+              <Link to="/pokemons">
             <Button variant="success" className="mt-3">Choose another Pokemon</Button>
         </Link>
-
-      </div>
-    );
-  }
+    </div>
+  );
+}
--- src/views/Pokemons.jsx
+++ src/views/Pokemons.jsx
@@ -1,11 +1,47 @@
-import Selector from "../components/Selector";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container"
+import axios from "axios";
 
 export default function Pokemons() {
-
-
+  const [pokeName, setPokeName] = React.useState("");
+  const navigate = useNavigate();
+  const goToPokemons = () => {
+    navigate(`/pokemons/${pokeName}`);
+  };
+  const [pokemons, setPokemons] = React.useState([]);
+  React.useEffect(() => {
+    const pokeData = async () => {
+      try {
+        const endpoint = "https://pokeapi.co/api/v2/pokemon?limit=151";
+        const response = await axios.get(endpoint);
+        setPokemons(response.data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    pokeData();
+  }, []);
   return (
     <div className="m-5">
-        <Selector />
+    <Container className="d-flex flex-column align-items-center">
+      <Form.Select
+        defaultValue=""
+        onChange={(e) => setPokeName(e.target.value)}
+      >
+        <option disabled value="">
+          Choose a pokemon!
+        </option>
+        {pokemons.map((pokemon) => (
+          <option key={pokemon.name} value={pokemon.name}>
+            {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+          </option>
+        ))}
+      </Form.Select>
+      <Button variant="danger" className="m-2" onClick={goToPokemons}>See Details</Button>
+    </Container>
     </div>
   );
-}
+        }
